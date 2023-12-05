4f0e5af539fb183f2f5715b3c4ce0e5f9f8024c0
thiaugpr <thiagoeu@gmail.com>
2023-12-05 16:19:17 -0300

    fix: remove scroll to top from movie details

--- src/components/MovieDetails/MovieDetailsContainer.js
+++ src/components/MovieDetails/MovieDetailsContainer.js
@@ -3,7 +3,7 @@ import MovieDetailsComponent from './MovieDetailsComponent';
 
 const MovieDetailsContainer = ({ ...props }) => {
   useEffect(() => {
-    document.documentElement.scrollTop = 0;
+    // document.documentElement.scrollTop = 0;
   }, []);
 
   return <MovieDetailsComponent {...props} />;
